89898b4dc3e78db6f1b389afaf5351ed9a5bba81
J. Smith <joe.a.smith@gmail.com>
2019-11-03 23:50:45 -0600

    template applied

---
+++ assets/js/init.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
+      
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
